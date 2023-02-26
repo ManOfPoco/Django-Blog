ec2c8ee8ed0cb87a6a3c01fe8d6b1286cbce8d3b
Kiril Kluchnikov <106924554+ManOfPoco@users.noreply.github.com>
2023-02-27 01:21:43 +0200

    Add: Category view, url, template

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from .views import (
-    post_detail
+    post_detail,
+    CategoryList
 )
 
 from django.views.generic import RedirectView
@@ -12,4 +13,6 @@ urlpatterns = [
     path('', RedirectView.as_view(pattern_name='home')),
     path('post-detail/<slug:slug>/<int:pk>',
          post_detail, name='blog_detail'),
+    path('category/', CategoryList.as_view(), name='categories'),
+    path('category/<slug:slug>', CategoryList.as_view(), name='category'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 
 from .models import (
-    Post,
+    Post, Category
 )
+
+from django.views.generic import ListView
 
 
 def post_detail(request, slug, pk):
@@ -14,3 +16,19 @@ def post_detail(request, slug, pk):
     }
 
     return render(request, 'blog/post_detail.html', context=context)
+
+
+class CategoryList(ListView):
+    model = Category
+    template_name = 'blog/category.html'
+    context_object_name = 'categories'
+
+    def get_queryset(self):
+        if self.kwargs.get('slug'):
+            query_set = get_list_or_404(
+                Post.objects.filter(
+                    category=get_object_or_404(
+                        Category, slug=self.kwargs['slug'])))
+        else:
+            query_set = Post.objects.all().order_by('-date_create')
+        return query_set

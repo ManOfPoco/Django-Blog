0cf6e0d803838ecdbca6531ae0dcaa956a6c53b2
Kiril Kluchnikov <106924554+ManOfPoco@users.noreply.github.com>
2023-02-27 00:58:47 +0200

    Add: Sign-up and sign-in templates; Add: routes to the sign-up and sign-in

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -16,11 +16,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from . import views
 
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls')),
+    path('users/', include('users.urls')),
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)

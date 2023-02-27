ba2b643091159a51489c0d8de9e0a030f596e05a
Kiril Kluchnikov <106924554+ManOfPoco@users.noreply.github.com>
2023-02-27 15:52:30 +0200

    Add: pagination to the category and user posts list

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -67,7 +67,7 @@ def post_detail(request, slug, pk):
 
     else:
 
-        post = get_object_or_404(Post, slug=slug, id=pk)
+        post = get_object_or_404(Post, slug=slug)
         comments = PostComment.objects.filter(
             post=post).order_by('-date_create')
 
@@ -80,50 +80,6 @@ def post_detail(request, slug, pk):
         }
 
         return render(request, 'blog/post_detail.html', context=context)
-
-
-class CategoryList(ListView):
-    model = Category
-    template_name = 'blog/category.html'
-    context_object_name = 'categories'
-
-    def get_queryset(self):
-        if self.kwargs.get('slug'):
-            query_set = get_list_or_404(
-                Post.objects.filter(
-                    category=get_object_or_404(
-                        Category, slug=self.kwargs['slug'])))
-        else:
-            query_set = Post.objects.all().order_by('-date_create')
-        return query_set
-
-
-class UserPostList(ListView):
-    model = Post
-    template_name = 'blog/user_posts.html'
-    context_object_name = 'user_posts'
-
-    def get_queryset(self):
-        queryset = Post.objects.filter(
-            author=Profile.objects.get(
-                slug=self.kwargs['slug']).user).order_by('-date_create')
-        return queryset
-
-
-class CreatePostView(LoginRequiredMixin, CreateView):
-    form_class = CreatePostForm
-    template_name = 'blog/post_create_form.html'
-
-    def form_valid(self, form: CreatePostForm):
-        form.instance.author = self.request.user
-        form.save()
-        return super().form_valid(form)
-
-
-class UpdatePostView(LoginRequiredMixin, UpdateView):
-    model = Post
-    form_class = CreatePostForm
-    template_name = 'blog/post_create_form.html'
 
 
 class DeletePostView(LoginRequiredMixin, DeleteView):
@@ -140,3 +96,47 @@ class DeleteCommentView(LoginRequiredMixin, DeleteView):
             'slug': comment.post.slug,
             'pk': comment.post.id
         })
+
+
+class UserPostList(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = Post.objects.filter(
+            author=Profile.objects.get(
+                slug=self.kwargs['slug']).user).order_by('-date_create')
+        return queryset
+
+
+class CategoryList(ListView):
+    model = Category
+    template_name = 'blog/category.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        if self.kwargs.get('slug'):
+            query_set = get_list_or_404(
+                Post.objects.filter(
+                    category=get_object_or_404(
+                        Category, slug=self.kwargs['slug'])))
+        else:
+            query_set = Post.objects.all().order_by('-date_create')
+        return query_set
+
+
+class CreatePostView(LoginRequiredMixin, CreateView):
+    form_class = CreatePostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form: CreatePostForm):
+        form.instance.author = self.request.user
+        form.save()
+        return super().form_valid(form)
+
+
+class UpdatePostView(LoginRequiredMixin, UpdateView):
+    model = Post
+    form_class = CreatePostForm
+    template_name = 'blog/post_form.html'

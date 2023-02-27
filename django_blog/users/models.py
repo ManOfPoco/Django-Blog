4cb06108e208e9c40cb9399f84426903f49c6286
Kiril Kluchnikov <106924554+ManOfPoco@users.noreply.github.com>
2023-02-27 14:09:11 +0200

    Add: Follow model for user following system

--- django_blog/users/models.py
+++ django_blog/users/models.py
@@ -12,8 +12,29 @@ class Profile(models.Model):
     about = models.CharField(max_length=255, blank=True, null=True)
     slug = models.SlugField(max_length=150)
 
+    @property
+    def followers_list(self):
+        return self.user.following.all().order_by('-follower')
+
+    @property
+    def following_list(self):
+        return self.user.follower.all().order_by('-following')
+
     def get_absolute_url(self):
         return reverse("profile", kwargs={"slug": self.slug})
 
     def __str__(self):
         return f'Profile {self.user.username} {self.user.last_name}'
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='follower')
+    following = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='following')
+
+    class Meta:
+        unique_together = ('follower', 'following')
+
+    def __str__(self):
+        return f'{self.follower} follows {self.following}'

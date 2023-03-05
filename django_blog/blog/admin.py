767cb9e73ad36b0d9f5c9882ff3f123864e23f51
Kiril Kluchnikov <106924554+ManOfPoco@users.noreply.github.com>
2023-03-05 22:24:51 +0200

    Customize: Django admin site; Add: filters, list_display and more...
    Add: basic filters for Post, Category; list_display for almost every model; search filters for almost every model with corresponding search fields

--- django_blog/blog/admin.py
+++ django_blog/blog/admin.py
@@ -2,22 +2,53 @@ from django.contrib import admin
 from .models import Post, Category, PostComment, PostLike
 
 
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'date_create', 'last_updated']
+
     fields = ['title', 'slug', 'body', 'author', 'category']
     prepopulated_fields = {'slug': ('title',)}
+    filter_horizontal = ('category',)
+
+    list_filter = ['author', 'category', 'date_create', 'last_updated']
+
+    search_fields = ['title', 'author__username']
+
+
+class CategoryNameFilter(admin.SimpleListFilter):
+    title = 'Name'
+    parameter_name = 'name'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('name', 'Category Name'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'name':
+            return queryset.order_by('name')
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
+    list_filter = [CategoryNameFilter]
+
+    search_fields = ['name']
 
 
 @admin.register(PostComment)
 class PostCommentAdmin(admin.ModelAdmin):
     fields = ['post', 'author', 'comment', 'parent']
+    list_display = ['post', 'author', 'date_create', 'last_updated', 'parent']
+
+    search_fields = ['author__username']
 
 
 @admin.register(PostLike)
 class PostLikeAdmin(admin.ModelAdmin):
     fields = ['post', 'author']
+    list_display = ['post', 'author', 'created']
+
+    search_fields = ['author__username']
--- django_blog/users/admin.py
+++ django_blog/users/admin.py
@@ -4,7 +4,10 @@ from .models import Profile, Follow
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'city', 'about']
     fields = ['user', 'city', 'about']
 
 
-admin.site.register(Follow)
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ['follower', 'following']
